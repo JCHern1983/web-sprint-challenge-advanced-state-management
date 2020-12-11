92a68ab95cfa90292f171716e22989f02cf41bc4
JCHern1983 <xnemesis21@gmail.com>
2020-12-11 10:23:13 -0800

    MVP

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,42 @@
-import axios from 'axios';
+import axios from "axios";
 
-//Task List:
-//1. Add fetch smurfs action: 
-//              - fetch and return initial list of smurfs
-//              - dispatch actions that indicate if we are waiting for a server response
-//              - dispatch an error text action if an error is returned from the server
-//2. Add add smurf action:
-//              - dispatch an error text action if smurf data does not includes a name, nickname and position field
-//              - send a post request with the smurf as body to see if there is an error
-//              - dispatch add smurf action if request is successful
-//              - dispatch an error text action if an request returns an error
-//3. Add set error text action:
-//              - return action object setting error text
-//4. Any other actions you deem nessiary to complete application.
+export const FETCH_DATA_START = "FETCH_DATA_START";
+export const FETCH_DATA_SUCCESS = "FETCH_DATA_SUCCESS";
+export const FETCH_DATA_FAILURE = "FETCH_DATA_FAILURE";
+
+export const POST_DATA_START = "POST_SMURF_START";
+export const POST_DATA_SUCCESS = "POST_SMURF_SUCCESS";
+export const POST_DATA_FAILURE = "POST_SMURF_FAILURE";
+
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: FETCH_DATA_START });
+
+    axios
+    .get("http://localhost:3333/smurfs")
+    .then(res => {
+        dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data });
+    })
+    .catch(err => {
+        console.log(err.response);
+        dispatch({
+            type: FETCH_DATA_FAILURE,
+            payload: `${err.response.status} ${err.response.data}`
+        });
+    });
+};
+
+export const addSmurf = newSmurf => dispatch => {
+
+    dispatch({ type: POST_DATA_START });
+    axios
+    .post("http://localhost:3333/smurfs", newSmurf)
+    .then(res => {
+        console.log("Post", res);
+        dispatch({ type: POST_DATA_SUCCESS, payload: res.data });
+    })
+        .catch(err => {
+        console.log(err);
+        dispatch({ type: POST_DATA_FAILURE, payload: err });
+    });
+};
